fe76b8d4df79521f7c9f75cffaf855c39485e505
tkyung05 <tkyung6761@gmail.com>
2023-11-11 03:06:22 +0900

    코드 간격 맞춤

--- level_1_공원_산책.js
+++ level_1_공원_산책.js
@@ -1,119 +1,123 @@
 // 프로그래머스 level 1 - 공원 산책 172928
 
-'use strict'
+'use strict';
 
 class Park {
-    static DIRECTION = { 
-        N: { Y: -1, X: 0 },
-        S: { Y: 1, X: 0 },
-        W: { Y: 0, X: -1 },
-        E: { Y: 0, X: 1 },
-    };
-    static SPOT = {
-        START: 'S',
-        ROAD: 'O',
-        OBSTRACLE: 'X',
-    };
-    
-    #map;
-    #height;
-    #width;
-    
-    constructor(map) {
-        this.#map = map;
-        this.#height = map.length;
-        this.#width = map[0].length;
-    }
-    
-    get height() {
-        return this.#height;
-    }
-    
-    get width() {
-        return this.#width;
-    }
-    
-    getSpot(positionY, positionX) {
-        return this.#map[positionY][positionX];
-    }
-    
-    getStartPosition() {
-        let position = null;
-        
-        this.#map.some((row, y) => {
-						const x = row.indexOf(Park.SPOT.START);
+  static DIRECTION = {
+    N: { Y: -1, X: 0 },
+    S: { Y: 1, X: 0 },
+    W: { Y: 0, X: -1 },
+    E: { Y: 0, X: 1 },
+  };
+  static SPOT = {
+    START: 'S',
+    ROAD: 'O',
+    OBSTRACLE: 'X',
+  };
 
-						if (x !== -1) {
-							return position = { y, x };
-						};
-	    	});
-			
-	    	return position;
-    }
+  #map;
+  #height;
+  #width;
+
+  constructor(map) {
+    this.#map = map;
+    this.#height = map.length;
+    this.#width = map[0].length;
+  }
+
+  get height() {
+    return this.#height;
+  }
+
+  get width() {
+    return this.#width;
+  }
+
+  getSpot(positionY, positionX) {
+    return this.#map[positionY][positionX];
+  }
+
+  getStartPosition() {
+    let position = null;
+
+    this.#map.some((row, y) => {
+      const x = row.indexOf(Park.SPOT.START);
+
+      if (x !== -1) {
+        return (position = { y, x });
+      }
+    });
+
+    return position;
+  }
 }
 
 class Dog {
-    #position;
-    
-    constructor(position) {
-        this.#position = position;
-    }
-    
-    get position() {
-        return this.#position;
-    }
-    
-    move(direction, steps) {
-        this.position.y += direction.Y * steps;
-        this.position.x += direction.X * steps;
-    }
+  #position;
+
+  constructor(position) {
+    this.#position = position;
+  }
+
+  get position() {
+    return this.#position;
+  }
+
+  move(direction, steps) {
+    this.position.y += direction.Y * steps;
+    this.position.x += direction.X * steps;
+  }
 }
 
 class Walk {
-    #target;
-    #place;
-    
-    constructor(target) {
-        this.#target = target;
+  #target;
+  #place;
+
+  constructor(target) {
+    this.#target = target;
+  }
+
+  placeAt(place) {
+    this.#place = place;
+  }
+
+  simulate(direction, steps) {
+    for (let step = 1; step <= steps; step++) {
+      const destinationY = this.#target.position.y + direction.Y * step;
+      const destinationX = this.#target.position.x + direction.X * step;
+
+      if (
+        destinationY >= this.#place.height ||
+        destinationX >= this.#place.width ||
+        destinationY < 0 ||
+        destinationX < 0
+      )
+        return false;
+
+      if (this.#place.getSpot(destinationY, destinationX) === Park.SPOT.OBSTRACLE) return false;
     }
-    
-    placeAt(place) {
-        this.#place = place;
-    }
-    
-    simulate(direction, steps) {        
-        for (let step = 1; step <= steps; step++) {
-            const destinationY = this.#target.position.y + (direction.Y * step);
-            const destinationX = this.#target.position.x + (direction.X * step);
-                        
-            if (destinationY >= this.#place.height || destinationX >= this.#place.width 
-|| destinationY < 0 || destinationX < 0) return false;
-                
-            if (this.#place.getSpot(destinationY, destinationX) === Park.SPOT.OBSTRACLE) return false;
-        };
-                
-        return true;
-    }
+
+    return true;
+  }
 }
 
 function solution(map, routes) {
-    const park = new Park(map);
-    const dog = new Dog( park.getStartPosition() );
-    const walk = new Walk(dog);
-    
-    walk.placeAt(park);
-    
-    routes.forEach((route) => {
-	    const direction = Park.DIRECTION[route[0]];
-        const steps = Number(route[2]);
-                
-        const isMovable = walk.simulate(direction, steps);        
-        
-        if (isMovable) {
-            dog.move(direction, steps);
-        };
-    });
-    
-    return [dog.position.y, dog.position.x];
-}
+  const park = new Park(map);
+  const dog = new Dog(park.getStartPosition());
+  const walk = new Walk(dog);
 
+  walk.placeAt(park);
+
+  routes.forEach((route) => {
+    const direction = Park.DIRECTION[route[0]];
+    const steps = Number(route[2]);
+
+    const isMovable = walk.simulate(direction, steps);
+
+    if (isMovable) {
+      dog.move(direction, steps);
+    }
+  });
+
+  return [dog.position.y, dog.position.x];
+}
--- level_1_달리기_경주.js
+++ level_1_달리기_경주.js
@@ -1,45 +1,45 @@
 // 프로그래머스 level 1 - 달리기 경주  178871
 
-'use strict'
+'use strict';
 
 class Commentator {
-    static #PLAYERS;
-    static #PLAYER_MAP;
-    
-    static initialize(players) {
-        this.#PLAYERS = players;
-        this.#PLAYER_MAP = new Map(players.map((name, sequence) => [name, sequence]));
-    }
-    
-    static getPlayers() {
-        return this.#PLAYERS;
-    }
-    
-    static getPlayerSequence(name) {
-        return this.#PLAYER_MAP.get(name);
-    }
-    
-    static #setPlayer(name, sequence) {
-        this.#PLAYERS[sequence] = name;
-        this.#PLAYER_MAP.set(name, sequence);
-    }
+  static #PLAYERS;
+  static #PLAYER_MAP;
 
-    static callPlayer(name, sequence) {
-        const overtakedName = this.#PLAYERS[sequence - 1];
-        
-        this.#setPlayer(name, sequence - 1);
-        this.#setPlayer(overtakedName, sequence);
-    }
+  static initialize(players) {
+    this.#PLAYERS = players;
+    this.#PLAYER_MAP = new Map(players.map((name, sequence) => [name, sequence]));
+  }
+
+  static getPlayers() {
+    return this.#PLAYERS;
+  }
+
+  static getPlayerSequence(name) {
+    return this.#PLAYER_MAP.get(name);
+  }
+
+  static #setPlayer(name, sequence) {
+    this.#PLAYERS[sequence] = name;
+    this.#PLAYER_MAP.set(name, sequence);
+  }
+
+  static callPlayer(name, sequence) {
+    const overtakedName = this.#PLAYERS[sequence - 1];
+
+    this.#setPlayer(name, sequence - 1);
+    this.#setPlayer(overtakedName, sequence);
+  }
 }
 
 function solution(players, callings) {
-    Commentator.initialize(players);
-    
-    callings.forEach((playerName) => {
-        const playerSequence = Commentator.getPlayerSequence(playerName);
-        
-        Commentator.callPlayer(playerName, playerSequence);
-    });
-    
-    return players;
+  Commentator.initialize(players);
+
+  callings.forEach((playerName) => {
+    const playerSequence = Commentator.getPlayerSequence(playerName);
+
+    Commentator.callPlayer(playerName, playerSequence);
+  });
+
+  return players;
 }
